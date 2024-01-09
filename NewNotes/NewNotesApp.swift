a68bcb851ea5252d3f30457554578f5b5ebcc685
Artur Uvarov <artur.uff@gmail.com>
2024-01-08 20:19:39 -0500

    Initial Commit

---
+++ NewNotes/NewNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewNotesApp.swift
+//  NewNotes
+//
+//  Created by Artur Uvarov on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct NewNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
